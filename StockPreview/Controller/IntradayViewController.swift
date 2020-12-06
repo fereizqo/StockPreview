d8ff704787d1d49d51c4574adff1b71791063334
Fereizqo Sulaiman <fereizqoahmad@sci.ui.ac.id>
2020-12-06 10:56:18 +0700

    feat: sorting function for intraday tableview

--- StockPreview/Controller/IntradayViewController.swift
+++ StockPreview/Controller/IntradayViewController.swift
@@ -16,6 +16,13 @@ class IntradayViewController: UIViewController {
     
     let repository = Repository(apiClient: APIClient())
     var timeSeriesArray1Min: [(String, TimeSeries1Min)] = []
+    var timeSeriesDict1Min: [String: TimeSeries1Min] = [:]
+    
+    let pickerView = UIPickerView()
+    var pickerToolBar = UIToolbar()
+    
+    private let interval = ["Open ↓","Open ↑","High ↓","High ↑","Low ↓","Low ↑"]
+    private var choosenInterval: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,9 +37,6 @@ class IntradayViewController: UIViewController {
         intradayTableView.dataSource = self
         
         // Appeareances
-//        symbolButton.titleLabel?.adjustsFontSizeToFitWidth = true
-//        symbolButton.titleLabel?.numberOfLines = 0
-//        symbolButton.titleLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping
         symbolButton.layer.cornerRadius = 5
         symbolButton.layer.borderWidth = 1
         symbolButton.layer.borderColor = UIColor.darkGray.cgColor
@@ -44,6 +48,7 @@ class IntradayViewController: UIViewController {
         firstLogin()
     }
     
+    // Setup appearance either symbor setted or not
     func firstLogin() {
         // Selected symbol is set
         if let stockSymbol = UserDefaults.standard.string(forKey: "StockSymbol") {
@@ -62,12 +67,17 @@ class IntradayViewController: UIViewController {
         
     }
     
+    // Get request for intraday data
     func getIntradayData(symbol: String) {
         repository.getIntradayData1Min(symbol: "IBM"){ result in
             switch result {
             case .success(let items):
-
-                for (key, value) in items.timeSeries1Min {
+                
+                // Store dict data
+                self.timeSeriesDict1Min = items.timeSeries1Min
+                
+                // Dictionary to Array
+                for (key, value) in self.timeSeriesDict1Min {
                     self.timeSeriesArray1Min.append((key,value))
                 }
 
@@ -116,10 +126,80 @@ class IntradayViewController: UIViewController {
     
     
     @IBAction func sortingButtonTapped(_ sender: UIButton) {
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        pickerView.backgroundColor = .white
+        pickerView.contentMode = .center
+        pickerView.frame = CGRect.init(x: 0.0, y: UIScreen.main.bounds.size.height - 300, width: UIScreen.main.bounds.size.width, height: 300)
+        self.view.addSubview(pickerView)
         
+        pickerToolBar = UIToolbar.init(frame: CGRect.init(x: 0, y: UIScreen.main.bounds.size.height - 300, width: UIScreen.main.bounds.size.width, height: 35))
+        pickerToolBar.barStyle = .default
+        pickerToolBar.items = [UIBarButtonItem.init(title: "Done", style: .done, target: self, action: #selector(onDonePickerButtonTapped))]
+        self.view.addSubview(pickerToolBar)
+    }
+    
+    @objc func onDonePickerButtonTapped() {
+        pickerView.removeFromSuperview()
+        pickerToolBar.removeFromSuperview()
+        if let choosenIntervals = choosenInterval {
+            // Remove array data
+            self.timeSeriesArray1Min.removeAll()
+            
+            // Set button title
+            sortingButton.setTitle("Sort by : \(choosenIntervals)", for: .normal)
+            
+            // Getting Selected Sorting Value
+            let param = choosenIntervals.split(separator: " ")[0]
+            
+            // Ascending order
+            if choosenIntervals.split(separator: " ")[1] == "↓" {
+                
+                if param == "Open" {
+                    for (key, value) in self.timeSeriesDict1Min.sorted(by: { $0.value.the1Open < $1.value.the1Open }) {
+                        self.timeSeriesArray1Min.append((key,value))
+                    }
+                } else if param == "High" {
+                    for (key, value) in self.timeSeriesDict1Min.sorted(by: { $0.value.the2High < $1.value.the2High }) {
+                        self.timeSeriesArray1Min.append((key,value))
+                    }
+                } else if param == "Low" {
+                    for (key, value) in self.timeSeriesDict1Min.sorted(by: { $0.value.the3Low < $1.value.the3Low }) {
+                        self.timeSeriesArray1Min.append((key,value))
+                    }
+                }
+                
+                // Reload tableview
+                intradayTableView.reloadData()
+            }
+            
+            // Descending order
+            else if choosenIntervals.split(separator: " ")[1] == "↑" {
+                
+                if param == "Open" {
+                    for (key, value) in self.timeSeriesDict1Min.sorted(by: { $0.value.the1Open > $1.value.the1Open }) {
+                        self.timeSeriesArray1Min.append((key,value))
+                    }
+                } else if param == "High" {
+                    for (key, value) in self.timeSeriesDict1Min.sorted(by: { $0.value.the2High > $1.value.the2High }) {
+                        self.timeSeriesArray1Min.append((key,value))
+                    }
+                    
+                } else if param == "Low" {
+                    for (key, value) in self.timeSeriesDict1Min.sorted(by: { $0.value.the3Low > $1.value.the3Low }) {
+                        self.timeSeriesArray1Min.append((key,value))
+                    }
+                }
+                
+                // Reload tableview
+                intradayTableView.reloadData()
+            }
+            
+        }
     }
 }
 
+// Tableview Delegate and Datasource
 extension IntradayViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let headerView = Bundle.main.loadNibNamed("IntradayHeaderTableViewCell", owner: self, options: nil)?.first as! IntradayHeaderTableViewCell
@@ -140,5 +220,24 @@ extension IntradayViewController: UITableViewDelegate, UITableViewDataSource {
         cell.lowLabel.text = value.the3Low
 
         return cell
+    }
+}
+
+// Pickerview Delegate and Datasource
+extension IntradayViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return interval.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return interval[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        choosenInterval = interval[row]
     }
 }
